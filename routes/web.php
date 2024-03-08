2e5286960c08c154801989d3f8396441f6ba9e66
Lcurents <laurentiusdika28@gmail.com>
2024-03-08 15:38:07 +0700

    update struktur folder

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,18 @@ Route::get('/Wisata', function () {
 Route::get('/Adat', function () {
     return view('Adat/index');
 });
+Route::get('/AirTerjun', function () {
+    return view('Wisata/AirTerjun/index');
+});
+Route::get('/Bukit', function () {
+    return view('Wisata/Bukit/index');
+});
+Route::get('/Danau', function () {
+    return view('Wisata/Danau/index');
+});
+Route::get('/Pantai', function () {
+    return view('Wisata/Pantai/index');
+});
+Route::get('/Sejarah', function () {
+    return view('Wisata/Sejarah/index');
+});
